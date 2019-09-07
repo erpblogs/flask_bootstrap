2c9be7bd66a0cde589ac9e79189fe23d6f5bcf7a
Quangtv <trinhvanquangf1@gmail.com>
2019-09-07 15:21:42 +0700

    Flask Templates
    build web page from html templates

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+# @app.route('/hello/<int:id>/')
+# def hello_world(**kwargs):
+#     return f"Hello <b>{'Quang' if kwargs.get('id', False) == 1 else 'Nhung'}</b>"
+# app.add_url_rule('/hello', 'hello', hello_world)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/success/<name>', methods=['POST', 'GET'])
+def success(name):
+    return f"Login success. Welcome {name}!"
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    user = 'N/A'
+    if request.method == 'POST':
+        user = request.form['nm']
+    else:
+        user = request.args.get('rm')
+    return redirect(url_for('success', name=user))
+
+
+@app.route('/guest/<name>')
+def guest(name):
+    return f"Hello {name}"
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    if name == 'admin':
+        return redirect(url_for('admin', name))
+    return redirect(url_for('guest', name))
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
